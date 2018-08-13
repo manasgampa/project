b371f0af3294f1bb4f2b82706dcfc42b661e1067
manas <manasgampa@gmail.com>
2018-08-13 10:39:39 -0400

    new project 2

--- ShoppingWeb/src/dao/HistoryDAO.java
+++ ShoppingWeb/src/dao/HistoryDAO.java
@@ -8,7 +8,7 @@ public interface HistoryDAO {
 	
 	//thÃªm má»›i má»™t lá»‹ch sá»­ mua hÃ ng.
 	public void addHistory(History h);
-	//this is a project
+	//this is a project new
 	//lá»�c lá»‹ch sá»­ cá»§a khÃ¡ch hÃ ng.
 	public List<History> getList(int user_id);
 
